b9fe595f8c94fae194f3dca6da246f68875bdcdf
BRUNO-RAMIREZ <89104842+BRUNO-RAMIREZ@users.noreply.github.com>
2023-01-13 18:55:52 -0400

    terminando la app

--- src/app/paises/pages/por-pais/por-pais.component.ts
+++ src/app/paises/pages/por-pais/por-pais.component.ts
@@ -5,16 +5,25 @@ import { PaisesService } from '../../services/paises.service';
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
-  styles: [],
+  styles: [
+    `
+      li {
+        cursor: pointer;
+      }
+    `,
+  ],
 })
 export class PorPaisComponent {
   termino: string = '';
   hayError: boolean = false;
   paises: Pais[] = [];
+  paisesSugeridos: Pais[] = [];
+  mostrarSugerencias: boolean = false;
 
   constructor(private paisesService: PaisesService) {}
 
   buscar(termino: string): void {
+    this.mostrarSugerencias = false;
     this.hayError = false;
     this.termino = termino;
     this.paisesService.buscarPais(termino).subscribe(
@@ -31,5 +40,15 @@ export class PorPaisComponent {
 
   sugerencias(termino: string) {
     this.hayError = false;
+    this.termino = termino;
+    this.mostrarSugerencias = true;
+    this.paisesService.buscarPais(termino).subscribe(
+      (paises) => (this.paisesSugeridos = paises.splice(0, 5)),
+      (err) => (this.paisesSugeridos = [])
+    );
+  }
+
+  buscarSugerido(termino: string) {
+    this.buscar(termino);
   }
 }
