a89ae3bf852520d4d2a46c668795905ddc05b91d
BRUNO-RAMIREZ <89104842+BRUNO-RAMIREZ@users.noreply.github.com>
2023-01-13 06:55:32 -0400

    estableciendo las rutas en el app-routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PaisesModule } from './paises/paises.module';
 import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, PaisesModule, SharedModule],
+  imports: [BrowserModule, AppRoutingModule, PaisesModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
